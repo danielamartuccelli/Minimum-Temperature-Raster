5820881c1d28ec4cdee92bb97d2b928a5c7addb4
anarciso90 <alfredo.narciso@unmsm.edu.pe>
2025-10-20 11:12:41 -0500

    update streamlit app

--- streamlit/src/streamlit_app.py
+++ streamlit/src/streamlit_app.py
@@ -83,5 +83,58 @@ with tab3:
 
 # TAB 4: Propuestas de política pública
 with tab4:
-     st.header("Propuestas de política pública ante el frío extremo") 
+     st.header("Propuestas de política pública ante el frío extremo")
+
+     st.markdown("""
+     Considerando el diagnóstico de distritos con menor temperatura promedio, se proponen las siguientes 3 intervenciones enfocadas en las localidades de la sierra sur y antiplánica del país.""") 
+
+     st.markdown("""
+    Política 1""")
+     
+     st.markdown("""
+    **Programa de Viviendas Térmicas Rurales (ISUR Ampliado)***""")
+     
+     st.markdown("""
+    Objetivo: Reducir en un treinta por cierto (30%) los casos de infecciones respiratorias agudas (IRA) en población infantil en zonas altoandinas con temperaturas promedio menor a 0 °C""")
+     
+     st.markdown("""
+    Territorio y población objetivo: Distritos rurales en Puno, Arequipa, Cusco, Tacna, Moquegua y Huancavelica identificados como los más fríos (temperatura anual promedio por debajo de los 0 grados). Población meta: 50 000 hogares rurales en altitudes""")
+     
+     st.markdown("""
+    Intervención""")
+     
+     st.markdown("""
+    1.Implementación de viviendas térmicamente mejoradas (paredes aislantes, techos dobles, sistemas pasivos de calefacción solar).""")
+     
+     st.markdown("""
+    2.Complementario al programa “Mi Abrigo” (FONCODES), pero con criterios geoespaciales basados en análisis raster.""")
+     
+     st.markdown("""
+    Costo estimado: S/ 8 000 por vivienda con una meta de 50 000 hogares. El costo total de la intervención asciende a S/ 400 millones.""")
+     
+     st.markdown("""
+    KPIn/
+    1.Disminucion en un 30 % de casos IRA en la población infantil en temporada de friaje (Fuente: ESSALUD/MINSA).n/
+    2. Aumento de un 20 % de confort térmico reportado por hogares (Fuente: SISFOH).n/
+    3. Aumento de un 15 % de asistencia escolar en invierno (Fuente: MINEDU).""")
+     
+     st.markdown("""
+    Política 2/n
+    ***Fondo de Adaptación Agropecuaria al Friaje***n/
+    Objetivo: Reducir pérdidas agrícolas y ganaderas en 25 % en distritos de la sierra sur expuestos a heladas recurrentes.n/
+    Territorio y población objetivo: Distritos agrícolas en Puno, Cusco y Arequipa con temperatura promedio menor a ≤ 0 °C (105 distritos). Población meta: 30 000 productores agropecuarios familiares.n/
+    Intervención:n/
+    1. Entrega de kits antiheladas (módulos de riego nocturno, mantas térmicas agrícolas).n/
+    2. Capacitación en calendarios agrícolas adaptativos.n/
+    3. Construcción de módulos de refugio ganadero para alpacas y ovinos.n/
+    Costo estimado: S/ 8 000 por agricultor con una meta de 30 000 trabajadores. El costo total de la intervención asciende a S/ 240 millones.n/
+    KPIn/
+    1. Disminuación en un 25 % de pérdidas reportadas en cultivos andinos (Fuente: MINAGRI).n/
+    2. Disminución en un 15 % de mortalidad de alpacas y ovinos en época de heladas(Fuente: SENASA).n/
+    3. 80% de agricultores capacitados adoptan prácticas agrícolas adaptadas al clima (Fuente: MINAGRI).""")
+     
+     st.markdown("""
+    Política 3""")
+
+
     
