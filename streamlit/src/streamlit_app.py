16fc0ba7811215ad988ee059f8538922ac444bbe
anarciso90 <alfredo.narciso@unmsm.edu.pe>
2025-10-19 23:55:39 -0500

    update streamlit app

--- streamlit/src/streamlit_app.py
+++ streamlit/src/streamlit_app.py
@@ -17,17 +17,17 @@ st.set_page_config(
 st.title("❄️🔥 Análisis del nivel de temperatura en los distritos del Perú utilizando imágenes raster")
 
 # Crear tabs
-tab1, tab2, tab3 = st.tabs(["📊 Distribución de temperaturas", "🥈 🏅 🏆 🥉 🥇 Ranking", "🌍 Mapas de distribución geográfica de temperaturas mínimas"])
+tab1, tab2, tab3, tab4 = st.tabs(["📊 Distribución de temperaturas", "🥈 🏅 🏆 🥉 🥇 Ranking", "🌍 Mapas de distribución geográfica de temperaturas mínimas", "Propuesta de política pública"])
 
 # TAB 1: Distribución de temperaturas 
 with tab1:
     st.header("📋 Distribución de temperaturas")
     
     # Mostrar imagen
-    st.image('streamlit/assets/01_distribucion.png', width='stretch')
+    st.image('streamlit/assets/01_distribucion.png', use_container_width=True)
 
     # Segunda imagen
-    st.image('streamlit/assets/01b_distribucion.png', width='stretch')
+    st.image('streamlit/assets/01b_distribucion.png', use_container_width=True)
     
     st.divider()
     
@@ -35,15 +35,15 @@ with tab1:
 
 # TAB 2: 🥈 🏅 🏆 🥉 🥇 Ranking
 with tab2:
-    st.header("🥇 Ranking de los distritos con temperaturas mínimas y máximas")
+    st.header("🥇 Ranking de los 15 distritos con temperaturas mínimas y máximas")
 
 
     # Mostrar imagen
-    st.image('streamlit/assets/02_ranking.png', width='stretch')
+    st.image('streamlit/assets/02_ranking.png', use_container_width=True)
 
 
     # Segunda imagen
-    st.image('streamlit/assets/01b_distribucion.png', width='stretch')
+    st.image('streamlit/assets/01b_distribucion.png', use_container_width=True)
 
 
     st.divider()
@@ -54,25 +54,32 @@ with tab3:
     st.header("🌍 Mapa de temperatura mínima por distrito")
 
     # Mostrar imagen
-    st.image('streamlit/assets/03_mapa.png', width='stretch')
+    st.image('streamlit/assets/03_mapa.png', use_container_width=True)
 
 
     # Botón para descargar CSV
     csv_path = 'data/estadisticas_completas.csv'  # Ajusta el nombre del archivo
     
     try:
-        with open(csv_path, 'rb') as f:
-            csv_data = f.read()
+                with open(csv_path, 'rb') as f:
+                    csv_data = f.read()
         
-        st.download_button(
-            label="📥 Descargar datos (CSV)",
-            data=csv_data,
-            file_name="datos_temperatura.csv",
-            mime="text/csv"
+                st.download_button(
+                    label="📥 Descargar datos (CSV)",
+                    data=csv_data,
+                    file_name="datos_temperatura.csv",
+                    mime="text/csv"
         )
     except FileNotFoundError:
         st.error(f"No se encontró el archivo: {csv_path}")
+    
+    
+    st.divider()
 
 
+# TAB 4: Propuestas de política pública
+with tab4:
+     st.header("Propuestas de política pública ante el frío extremo") 
 
+     st.markdown()
     
